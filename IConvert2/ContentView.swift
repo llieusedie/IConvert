669fc4ddae6307d9243fd65f08f46e3e7d9dce3e
llieusedie <90383958+llieusedie@users.noreply.github.com>
2022-04-11 21:31:14 +0300

    Initial Commit

---
+++ IConvert2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IConvert2
+//
+//  Created by Paul Kirnoz on 11.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IConvert2/IConvert2App.swift
@@ -0,0 +1,17 @@
+//
+//  IConvert2App.swift
+//  IConvert2
+//
+//  Created by Paul Kirnoz on 11.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct IConvert2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
